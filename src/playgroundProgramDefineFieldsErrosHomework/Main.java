934804de212b78d76d2dbfd8f73b15826a23250b
jwdukes <jwdukes@gmail.com>
2021-01-28 11:15:50 -0600

    first commit

---
+++ src/playgroundProgramDefineFieldsErrosHomework/Main.java
@@ -0,0 +1,17 @@
+package playgroundProgramDefineFieldsErrosHomework;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String firstName; //string cannot equal String break
+		Integer classMembers;
+		if (true) {
+			int numbers = 5; //int cannot be named int final or int class
+		}
+		System.out.println(number);  // unresolved 
+		
+		
+		
+	}
+
+}
